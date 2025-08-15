ab002d24008f9a141227796a533a02ecd97ead2c
Murilow <milanmurilo7@gmail.com>
2025-08-15 11:50:55 -0300

    Meu site com variações

---
+++ src/data/bruxos.js
@@ -0,0 +1,82 @@
+export default [
+{
+    id: 1,
+    nome: "Neville",
+    casa: "Grifinória",
+    ano: "7",
+    varinha: "Cerejeira",
+    patrono: "Desconhecido"
+},
+{
+    id: 2,
+    nome: "Manoel Gomes",
+    casa: "Sonserina",
+    ano: "6", 
+    varinha: "Carvalho",
+    patrono: "sapo"
+},
+{
+    id: 3,
+    nome: "Rafael Junior",
+    casa: "Sonserina",
+    ano: "7",
+    varinha: "Salgueiro",
+    patrono: "Coruja"
+},
+{
+    id: 4,
+    nome: "Carlos Minino",
+    casa: "Lufa-Lufa",
+    ano: "6",
+    varinha: "Abeto",
+    patrono: "Dragão"
+},
+{
+    id: 5,
+    nome: "Enzo Junior",
+    casa: "Corvinal",
+    ano: "6",
+    varinha: "Orvalho",
+    patrono: "Ovelha"
+},
+{
+    id: 6,
+    nome: "Draco Malfoy",
+    casa: "Sonserina",
+    ano: "7",
+    varinha: "Bananeira",
+    patrono: "Tubarão Martelo"
+},
+{
+    id: 7,
+    nome: "Junior Mendes",
+    casa: "Corvinal",
+    ano: "7",
+    varinha: "Freixo",
+    patrono: "Peixe Dourado"
+},
+{
+    id: 8,
+    nome: "Cho Chang",
+    casa: "Corvinal",
+    ano: "6",
+    varinha: "Chifre de Unicornio",
+    patrono: "Aranha"
+},
+{
+    id: 9,
+    nome: "Cedricco Diggoroy",
+    casa: "Lufa-Lufa",
+    ano: "7",
+    varinha: "Nogueira",
+    patrono: "Lagarto"
+},
+{
+    id: 10,
+    nome: "João Limonada",
+    casa: "Grifinória",
+    ano: "7",
+    varinha: "Betla",
+    patrono: "Mosca"
+}
+];
